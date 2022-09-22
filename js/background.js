780fb8585e0f546206e6cf8b715c06b35a252ffe
kyo-ago <kyo.ago@gmail.com>
2022-09-22 11:15:15 +0900

    update v3

--- js/background.js
+++ js/background.js
@@ -1,81 +1,69 @@
 "use strict";
 
-(function () {
-	chrome.contextMenus.create({
-		'id' : 'zenkaku',
-		'title' : '全角へ変換する',
-		'contexts' : ['editable']
-	});
-	chrome.contextMenus.create({
-		'id' : 'hankaku',
-		'title' : '半角へ変換する',
-		'contexts' : ['editable']
-	});
-	chrome.contextMenus.create({
-		'id' : 'katakana',
-		'title' : 'カナへ変換する',
-		'contexts' : ['editable']
-	});
-	var executeScript = {
-		'zenkaku' : function () {
-			var active = document.activeElement;
-			if (!active) {
-				return;
-			}
-			if (active.tabName !== 'textarea' && active.type !== 'text') {
-				return;
-			}
-			if (!active.value) {
-				return;
-			}
-			active.value = active.value.replace(/[\w -]/g, function(s) {
-				return ({
-					' ' : '　'
-				})[s] || String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
-			});
-		},
-		'hankaku' : function () {
-			var active = document.activeElement;
-			if (!active) {
-				return;
-			}
-			if (active.tabName !== 'textarea' && active.type !== 'text') {
-				return;
-			}
-			if (!active.value) {
-				return;
-			}
-			active.value = active.value.replace(/[Ａ-Ｚａ-ｚ０-９＿　ー]/g, function(s) {
-				return ({
-					'　' : ' '
-				})[s] || String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-			});
-		},
-		'katakana' : function () {
-			var active = document.activeElement;
-			if (!active) {
-				return;
-			}
-			if (active.tabName !== 'textarea' && active.type !== 'text') {
-				return;
-			}
-			if (!active.value) {
-				return;
-			}
-			active.value = active.value.replace(/[ぁ-ん]/g, function(s) {
-				return String.fromCharCode(s.charCodeAt(0) + 0x60);
-			});
-		}
-	};
-	chrome.contextMenus.onClicked.addListener(function (info, tab) {
-		if (!tab) {
-			return;
-		}
-		if (!executeScript[info.menuItemId]) {
-			return;
-		}
-		chrome.tabs.executeScript(tab.id, {
-			'code' : '( ' + executeScript[info.menuItemId] + ' )()'
-		});
-	});
-})();
+const register = async () => {
+    await chrome.contextMenus.removeAll();
+    chrome.contextMenus.create({
+        'id': 'zenkaku',
+        'title': '全角へ変換する',
+        'contexts': ['editable']
+    });
+    chrome.contextMenus.create({
+        'id': 'hankaku',
+        'title': '半角へ変換する',
+        'contexts': ['editable']
+    });
+    chrome.contextMenus.create({
+        'id': 'katakana',
+        'title': 'カナへ変換する',
+        'contexts': ['editable']
+    });
+};
+chrome.runtime.onInstalled.addListener(register);
+chrome.runtime.onStartup.addListener(register);
+
+const executeScriptFunc = (menuItemId) => {
+    const active = document.activeElement;
+    if (!active) {
+        return;
+    }
+    if (active.tabName !== 'textarea' && active.type !== 'text') {
+        return;
+    }
+    if (!active.value) {
+        return;
+    }
+
+    const exec = {
+        'zenkaku': (value) =>
+            value.replace(/[\w -]/g, (s) => ({
+                ' ': '　'
+            })[s] || String.fromCharCode(s.charCodeAt(0) + 0xFEE0))
+        ,
+        'hankaku': (value) =>
+            value.replace(/[Ａ-Ｚａ-ｚ０-９＿　ー]/g, (s) =>
+                ({
+                    '　': ' '
+                })[s] || String.fromCharCode(s.charCodeAt(0) - 0xFEE0)
+            )
+        ,
+        'katakana': (value) =>
+            value.replace(
+                /[ぁ-ん]/g, (s) =>
+                    String.fromCharCode(s.charCodeAt(0) + 0x60)
+            )
+    };
+    active.value = exec[menuItemId]?.(active.value);
+};
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (!tab) {
+        return;
+    }
+
+    chrome.scripting.executeScript(
+        {
+            target: {tabId: tab.id},
+            args: [info.menuItemId],
+            func: executeScriptFunc,
+        }
+    );
+});
